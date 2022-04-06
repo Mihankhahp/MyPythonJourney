64c75e401a94248ac9f083119e89d1c20dc8cfa1
Peyman <mihankhahp@gmail.com>
2022-04-06 21:14:10 +0430

    Update GestureVolumeControl.py

--- Project1_GestureVolumeControl/GestureVolumeControl.py
+++ Project1_GestureVolumeControl/GestureVolumeControl.py
@@ -14,14 +14,22 @@ wCam, hCam = 640, 480
 
 detector = htm.handDetector(detectionCon=0.7)
 
+
+# initialization code from the library
+
 devices = AudioUtilities.GetSpeakers()
 interface = devices.Activate(
     IAudioEndpointVolume._iid_, CLSCTX_ALL, None)
 volume = cast(interface, POINTER(IAudioEndpointVolume))
-volume.GetMute()
-volume.GetMasterVolumeLevel()
-volume.GetVolumeRange()
-volume.SetMasterVolumeLevel(-20.0, None)
+
+# volume.GetMute()
+# volume.GetMasterVolumeLevel()
+
+
+volRange = volume.GetVolumeRange()  # fom -74 - 0
+minVol = volRange[0]
+maxVol = volRange[1]
+
 
 cap = cv2.VideoCapture(0)
 cap.set(3, wCam)
@@ -34,7 +42,13 @@ while True:
     img = detector.findHands(img)
     # Its already drawing in findHands
     lmList = detector.findPosition(img, draw=False)
+    vol = 0
+    volBar = 400
+    volPerc = 0
+
     if len(lmList) != 0:
+        # print("ittttttttttttttttttttttttttttts", lmList)
+
         # print(lmList[4], lmList[8])
         # The point of Tomb and Index finger
         x4, y4 = lmList[4][1], lmList[4][2]
@@ -49,11 +63,19 @@ while True:
         cv2.circle(img, (cx, cy), 7, (255, 0, 255), cv2.FILLED)
 
         length = math.hypot(x8-x4, y8-y4)
+        vol = np.interp(length, [15, 114], [minVol, maxVol])
+        volBar = np.interp(length, [15, 114], [400, 150])
+        volPerc = np.interp(length, [15, 114], [0, 100])
+        volume.SetMasterVolumeLevel(vol, None)
 
-        if length < 30:
+        if length < 25:
             cv2.circle(img, (cx, cy), 7, (255, 0, 0), cv2.FILLED)
-
-        print(length)
+    cv2.rectangle(img, (50, 150), (85, 400), (0, 255, 0), 3)
+    cv2.rectangle(img, (50, int(volBar)), (85, 400), (0, 255, 0), cv2.FILLED)
+    cv2.putText(img, f'{int(volPerc)}%', (40, 450),
+                cv2.FONT_HERSHEY_PLAIN, 3, (0, 255, 0), 3)
+    # print(length)
+    # print("fin")
 
     cTime = time.time()
     fps = 1/(cTime-pTime)
