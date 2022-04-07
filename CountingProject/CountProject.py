16209984654e34873b423658c1b5570b4d37612d
Peyman <mihankhahp@gmail.com>
2022-04-07 21:06:48 +0430

    Update CountProject.py

--- CountingProject/CountProject.py
+++ CountingProject/CountProject.py
@@ -30,15 +30,14 @@ while True:
 
     #  Find the hands
     img = detector.findHands(img)
-    lmList = detector.findPosition(img, draw=False)
+    lmList, handNo = detector.findPosition(img, draw=False)
 
+    fingers = []
     if len(lmList) != 0:
-        fingers = []
-
         #  Find Which hands are up
         if lmList[tipsIds[0]][1] > lmList[tipsIds[4]-3][1]:  # detect right hands
-            
-            print("Now right hand is up")
+
+            print("Now right hand is up and the count is: ")
             # Thumb detector
             if lmList[tipsIds[0]][1] > lmList[tipsIds[0]-1][1]:
                 fingers.append(1)
@@ -55,8 +54,8 @@ while True:
             totalFingers = fingers.count(1)
             print(totalFingers)
 
-        else:
-            print("Now Left hand is up")
+        else:  # detect left hands
+            print("Now Left hand is up and the count is: ")
             if lmList[tipsIds[0]][1] < lmList[tipsIds[0]-1][1]:
                 fingers.append(1)
             else:
@@ -72,8 +71,11 @@ while True:
             totalFingers = fingers.count(1)
             print(totalFingers)
 
-    h, w, c = overlayList[0].shape
-    img[0:h, 0:w] = overlayList[0]
+        h, w, c = overlayList[totalFingers-1].shape
+        img[0:h, 0:w] = overlayList[totalFingers-1]
+        cv2.rectangle(img, (20, 225), (170, 425), (0, 255, 0),cv2.FILLED)
+        cv2.putText(img, str(totalFingers), (45, 375),
+                cv2.FONT_HERSHEY_PLAIN, 10, (255, 0, 0), 25)
 
     cTime = time.time()
     fps = 1/(cTime-pTime)
@@ -82,4 +84,4 @@ while True:
     cv2.putText(img, f'FPS: {int(fps)}', (400, 70),
                 cv2.FONT_HERSHEY_PLAIN, 3, (0, 255, 0), 3)
     cv2.imshow("Counting Projects", img)
-    cv2.waitKey(1)
+    cv2.waitKey(1000)
