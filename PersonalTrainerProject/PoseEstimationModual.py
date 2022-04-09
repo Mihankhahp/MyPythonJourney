668c05d168426923579cb522d79a6d76baddc576
Peyman <mihankhahp@gmail.com>
2022-04-09 17:22:23 +0430

    Create PoseEstimationModual.py

---
+++ PersonalTrainerProject/PoseEstimationModual.py
@@ -0,0 +1,71 @@
+import cv2
+import mediapipe as mp
+import time
+
+
+class poseDetector():
+
+    def __init__(self, mode=False, model_complexity=1, upBody=False, smooth=True, detectionCon=0.5, trackCon=0.5):
+        self.mode = mode
+        self.model_complexity = model_complexity
+        self.upBody = upBody
+        self.smooth = smooth
+        self.detectionCon = detectionCon
+        self.trackCon = trackCon
+        self.mpDraw = mp.solutions.drawing_utils
+        self.mpPose = mp.solutions.pose
+        self.pose = self.mpPose.Pose(
+            self.mode, self.model_complexity, self.upBody, self.smooth, self.detectionCon, self.trackCon)
+
+    def findPose(self, img, draw=True):
+
+        imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+
+        self.results = self.pose.process(imgRGB)
+        if self.results.pose_landmarks:
+            if draw:
+                self.mpDraw.draw_landmarks(img, self.results.pose_landmarks,
+                                           self.mpPose.POSE_CONNECTIONS)
+        return img
+
+    def getPosition(self, img, personsDetected=0, draw=True):
+        lmList = []
+        if self.results.pose_landmarks:
+            for id, lm in enumerate(self.results.pose_landmarks.landmark):
+                h, w, c = img.shape
+                cx, cy = int(lm.x*w), int(lm.y * h)
+                lmList.append([id, cx, cy])
+                if draw:
+                    cv2.circle(img, (cx, cy), 5, (255, 0, 0), cv2.FILLED)
+
+        return lmList
+
+
+def main():
+    cap = cv2.VideoCapture(0)
+    pTime = 0
+    detector = poseDetector()
+
+    while True:
+        success, img = cap.read()
+        img = detector.findPose(img)
+        lmList = detector.getPosition(img)
+        if len(lmList) != 0:
+            cv2.circle(img, (lmList[0][1], lmList[0][2]),
+                       7, (255, 0, 255), cv2.FILLED)
+            print("The location is : ", lmList[0])
+
+        cTime = time.time()
+        fps = 1/(cTime-pTime)
+        pTime = cTime
+
+        cv2.putText(img, str(int(fps)), (70, 50),
+                    cv2.FONT_HERSHEY_PLAIN, 3, (255, 0, 255), 3)
+
+        imgResized = cv2.resize(img, (960, 540))
+        cv2.imshow("PoseEstimate", imgResized)
+        cv2.waitKey(100)
+
+
+if __name__ == "__main__":
+    main()
