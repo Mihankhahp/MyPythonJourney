f1506603dab2513b4c32953a570808a9901165c3
Peyman <mihankhahp@gmail.com>
2022-04-09 20:02:12 +0430

    Update PoseEstimationModual.py

--- PersonalTrainerProject/PoseEstimationModual.py
+++ PersonalTrainerProject/PoseEstimationModual.py
@@ -1,6 +1,8 @@
 import cv2
+from cv2 import line
 import mediapipe as mp
 import time
+import math
 
 
 class poseDetector():
@@ -29,16 +31,41 @@ class poseDetector():
         return img
 
     def getPosition(self, img, personsDetected=0, draw=True):
-        lmList = []
+        self.lmList = []
         if self.results.pose_landmarks:
             for id, lm in enumerate(self.results.pose_landmarks.landmark):
                 h, w, c = img.shape
                 cx, cy = int(lm.x*w), int(lm.y * h)
-                lmList.append([id, cx, cy])
+                self.lmList.append([id, cx, cy])
                 if draw:
                     cv2.circle(img, (cx, cy), 5, (255, 0, 0), cv2.FILLED)
 
-        return lmList
+        return self.lmList
+
+    # Find angle between the point number 1, 2, 3
+    def findAngle(self, img, pointList, draw=True):
+        drawList = []
+
+        for point in pointList:
+            i, x, y = self.lmList[point][0:]
+            cv2.circle(img, (x, y), 10, (0, 255, 0), cv2.FILLED)
+            cv2.circle(img, (x, y), 15, (0, 255, 0), 2)
+            drawList.append([i, x, y])
+
+        drawList.sort()
+        if draw:
+            n = len(drawList)
+            for i in range(n):
+                if i+1 == n:
+                    break
+                cv2.line(
+                    img, drawList[i][1:], drawList[i+1][1:], (0, 0, 255), 3)
+
+        # Calculate the Angle:
+        angle = math.degrees(math.atan2((drawList[-1][2])-(drawList[-2][2]), (drawList[-1][1])-(
+            drawList[-2][1]))-math.atan2((drawList[0][2])-(drawList[-2][2]), (drawList[0][1])-(drawList[-2][1])))
+        cv2.putText(img, str(int(angle)), ((
+            drawList[1][1]-50), (drawList[1][2]+50)), cv2.FONT_HERSHEY_PLAIN, 2, (0, 0, 255), 2)
 
 
 def main():
